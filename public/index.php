3aa7074ba73b6593696dccab590068a5a81fa05e
cubes1 <cubes1@cubes1-Lenovo-E50-80>
2016-07-20 10:03:50 +0200

    commit cubes-cms

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+    
+    echo "Test";
+    die();
 
     // Define path to application directory
     defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
