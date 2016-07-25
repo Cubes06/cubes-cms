9235510acd5e3137e5972764827f0bb4e8902ff1
Milomir Dragovic <milomir.drago@gmail.com>
2016-07-25 09:37:23 +0200

    pon 24++.07.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,7 +3,15 @@
     class IndexController extends Zend_Controller_Action {
 
         public function init() {
-            /* Initialize action controller here */
+            $cmsClientsDbTable = new Application_Model_DbTable_CmsClients();
+            
+            $select = $cmsClientsDbTable->select();
+            $select->order('order_number ASC');
+                   
+            $clients = $cmsClientsDbTable->fetchAll($select);
+            
+            $this->view->clients = $clients;
+            $this->view->systemMessages = $systemMessages;
         }
 
         public function indexAction() {
